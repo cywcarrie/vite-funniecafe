383b6b338f4f8e7511234435c6c3a55c7633be48
chcarrie <ppinkywei@gmail.com>
2026-01-15 18:08:45 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     tailwindcss(),
     vueDevTools(),
   ],
+  base: '/vite-funniecafe/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
